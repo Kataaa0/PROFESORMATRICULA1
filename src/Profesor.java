0d6f208efd537403d4d9b44859973b10397fbbb0
Kataaa0 <129550235+Kataaa0@users.noreply.github.com>
2023-10-23 19:05:19 -0500

    Update Profesor.java

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,17 +1,16 @@
 public class Profesor {
-    // Atributos del profesor
+   
     private String nombre;
     private String cedula;
     private String codigoP;
 
-    // Constructor
     public Profesor(String nombre, String cedula, String codigoP) {
         this.nombre = nombre;
         this.cedula = cedula;
         this.codigoP = codigoP;
     }
 
-    // Getters
+
     public String getNombre() {
         return nombre;
     }
